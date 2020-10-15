267d0e910302d8d9c89a1552179e22ba6e7987c4
Dylan <dylancooper44@gmail.com>
2020-10-14 21:51:14 -0400

    First Commit

---
+++ Lab 3 Again/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int realNumber = 0;
+            bool stillPlaying = true;
+
+            Console.WriteLine("Hello! Please Enter Your Name");
+            string userName = Console.ReadLine();
+            while (stillPlaying)
+            {
+                Console.WriteLine("Hello {0}! Please Enter a Positive Number Between 1-100", userName);
+                string userNumber = Console.ReadLine();
+                bool validInput = int.TryParse(userNumber, out realNumber);
+
+
+                if (validInput == false)
+                {
+                    Console.WriteLine("{0}, Your Entry of {1} is Invalid. Please Enter a Positive Number Between 1 and 100", userName, userNumber);
+                    continue;
+                }
+
+                else if (realNumber < 1 || realNumber > 100)
+                {
+                    Console.WriteLine("{0}, Your Entry of {1} is Invalid.Please Enter a Positive Number Between 1 and 100", userName, userNumber);
+                    continue;
+                }
+                else
+                {
+                    if (realNumber % 2 > 0)
+                    {
+                        Console.WriteLine("{0} Odd.", userNumber);
+                        stillPlaying = false;
+                    }
+                    else if (realNumber % 2 == 0 && (realNumber > 0 && realNumber <= 25))
+                    {
+                        Console.WriteLine("Even and less than 25.");
+                        stillPlaying = false;
+                    }
+                    else if (realNumber % 2 == 0 && (realNumber > 25 && realNumber <= 60))
+                    {
+                        Console.WriteLine("Even.");
+                        stillPlaying = false;
+                    }
+                    else if (realNumber % 2 == 0 && (realNumber > 25 && realNumber <= 60))
+                    {
+                        Console.WriteLine("Even.");
+                        stillPlaying = false;
+                    }
+                    Console.WriteLine("Would You Like To Play Again?");
+
+                    string userBored = Console.ReadLine();
+
+                    if (userBored.Equals("yes", StringComparison.OrdinalIgnoreCase))
+                    {
+                        stillPlaying = true;
+                        continue;
+                    }
+                    else
+                    {
+                        stillPlaying = false;
+                    }
+
+                }
+                Console.WriteLine("Thank You {0} For Playing!", userName);
+            }
+        }
+    }
+}
